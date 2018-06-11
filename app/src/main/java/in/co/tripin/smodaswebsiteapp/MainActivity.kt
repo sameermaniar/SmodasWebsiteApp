48a756c9bc9f178b1fc8152ef2ac6f9dd41b953a
ravishankar.ahirwar <ravishankar.ahirwar@gmail.com>
2018-06-11 19:56:54 +0530

    Initial commit

---
+++ app/src/main/java/in/co/tripin/smodaswebsiteapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package `in`.co.tripin.smodaswebsiteapp
+
+import android.annotation.SuppressLint
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webview.settings.javaScriptEnabled = true
+        webview.loadUrl("https://smodas.wooplr.com/")
+
+    }
+}
